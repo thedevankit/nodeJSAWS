7a4bdb9451f46b72e7ccfd121eda5bd84ad8c944
Sunil Varia <sunilvaria@gmail.com>
2021-09-22 12:40:03 -0700

    second commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/mysubzi/', (req, res) => {
+  res.send('Hello World! would you like to buy some tomatoes?')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
